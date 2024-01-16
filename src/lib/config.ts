317ac142fd39a55b4aa0ec021352c31705a6fdff
slivingston <spencer@srlivingston.com>
2024-01-15 20:17:45 -0600

    initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = "Spencer's Projects"
+export const description = "A location for documenting my creative, professional, and research projects"
+export const url = dev ? "http://locahost:5173" : ""
