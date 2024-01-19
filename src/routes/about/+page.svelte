e3f44afe0a12da0ef8468d8bd7a3068d63a667e4
slivingston <spencer@srlivingston.com>
2024-01-18 20:54:32 -0600

    styling and about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
 <h1>About</h1>
 <o>I'm Spencer Livingston, I've been working as a software developer for several years. 
-    I'm currently working full time while pursing my Master's in Geography studying extreme floods and droughts.</o>
+    I'm currently working full time while pursing my Master's in Geography studying extreme floods and droughts. I also enjoy the outdoors and making music</o>
